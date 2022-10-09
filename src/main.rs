a7948211c5d9f585c967076298561853f142761f
mhj <5261387+markhj@users.noreply.github.com>
2022-10-09 11:09:33 +0200

    Renamed to look closer to native Option

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,42 @@
 use std::panic::catch_unwind;
-use crate::CustomOption::*;
+use crate::COption::*;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-enum CustomOption<T> {
-    CustomNone,
-    CustomSome(T),
+enum COption<T> {
+    CNone,
+    CSome(T),
 }
 
-impl<T> CustomOption<T> {
+impl<T> COption<T> {
     pub fn unwrap(self) -> T {
         match self {
-            CustomSome(v) => v,
-            CustomNone => panic!("Cannot unwrap `OptNone`"),
+            CSome(v) => v,
+            CNone => panic!("Cannot unwrap `None`"),
         }
     }
     pub fn is_some(&self) -> bool {
-        matches!(*self, CustomSome(_))
+        matches!(*self, CSome(_))
     }
     pub fn is_none(&self) -> bool {
         !self.is_some()
     }
 }
 
-fn example(some: bool) -> CustomOption<String> {
+fn example(some: bool) -> COption<String> {
     match some {
-        true => CustomSome(String::from("Hello world")),
-        false => CustomNone,
+        true => CSome(String::from("Hello world")),
+        false => CNone,
     }
 }
 
 fn main() {
-    let some: CustomOption<String> = example(true);
-    let none: CustomOption<String> = example(false);
+    let some: COption<String> = example(true);
+    let none: COption<String> = example(false);
 
     assert!(some.is_some());
     assert!(!some.is_none());
     assert!(!none.is_some());
     assert!(none.is_none());
     assert_eq!("Hello world", some.unwrap());
-
-    // This will display output in the console, which may look like the
-    // application has failed, but as long as the test summary is successful,
-    // it's no cause for concern
     assert!(catch_unwind(|| none.unwrap()).is_err());
 }
